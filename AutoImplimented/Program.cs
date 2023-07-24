acb3f06db1d2e3e896fdc37f87df9e54572266b4
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-07-24 13:51:56 +0530

    Add project files.

---
+++ AutoImplimented/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoImplimented
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.rn = 101;
+            student.name = "Vidyashree";
+            student.percentage = 78;
+
+            Console.WriteLine(student.rn);
+            
+        }
+
+    }
+}
---
+++ AutoImplimented/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoImplimented
+{
+    public class Student
+    {
+        private int rn { get; set; }
+        private string name { get; set; }
+        private double percentage { get; set; }
+
+        public string PrintStudent()
+        {
+            return $"RollNo={rn},Name={name},Percentage={percentage}";
+    }
+}
